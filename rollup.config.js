5e4b9346af19a9bd8316bff5eb8fec1189fe8631
dmaynard <davidsmaynard@gmail.com>
2019-05-22 15:09:14 -0700

    fix production build
    configures the proper uglify packacge so that the output is minimized.  It is not yet uglified

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve';
-import { uglify } from 'rollup-plugin-uglify';
-
+import uglify from 'rollup-plugin-uglify-es';
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
@@ -9,11 +8,17 @@ export default {
 	input: 'src/main.js',
 	output: {
 		file: 'public/bundle.js',
-		format: 'esm', 
+		format: 'esm',
 	  sourcemap: true
 	},
 	plugins: [
 		resolve(), // tells Rollup how to find date-fns in node_modules
-		production && uglify() // minify, but only in production
+		// production && uglify({
+    // compress: {
+    //   toplevel: true
+          //},
+	   //	mangle: {}
+      // }) // minify, but only in production
+			production && uglify()
 	]
 };
